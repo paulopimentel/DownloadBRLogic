aeba6e3212fe1094d8a9229ff600a9579d409665
Paulo Pimentel <paulo.santos@iob.com.br>
2023-02-12 01:51:03 -0300

    First commit

---
+++ DownloadBRLogic/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace DownloadBRLogic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblInicio = new System.Windows.Forms.Label();
+            this.lblProgresso = new System.Windows.Forms.Label();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lblSelecao = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(0, 49);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(1289, 806);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://app.brlogic.com/r/8008f7c457/radio/radio-file-manager/#directory-57578", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(0)))), ((int)(((byte)(114)))));
+            this.panel1.Controls.Add(this.lblInicio);
+            this.panel1.Controls.Add(this.lblProgresso);
+            this.panel1.Controls.Add(this.btnExecutar);
+            this.panel1.Controls.Add(this.lblSelecao);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1297, 51);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblInicio
+            // 
+            this.lblInicio.AutoSize = true;
+            this.lblInicio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(0)))), ((int)(((byte)(114)))));
+            this.lblInicio.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInicio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblInicio.Location = new System.Drawing.Point(856, 27);
+            this.lblInicio.Name = "lblInicio";
+            this.lblInicio.Size = new System.Drawing.Size(124, 14);
+            this.lblInicio.TabIndex = 9;
+            this.lblInicio.Text = "Iniciado às 23:00h";
+            this.lblInicio.Visible = false;
+            // 
+            // lblProgresso
+            // 
+            this.lblProgresso.AutoSize = true;
+            this.lblProgresso.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(0)))), ((int)(((byte)(114)))));
+            this.lblProgresso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgresso.ForeColor = System.Drawing.Color.Yellow;
+            this.lblProgresso.Location = new System.Drawing.Point(854, 7);
+            this.lblProgresso.Name = "lblProgresso";
+            this.lblProgresso.Size = new System.Drawing.Size(212, 20);
+            this.lblProgresso.TabIndex = 8;
+            this.lblProgresso.Text = "Baixando x de xx músicas";
+            this.lblProgresso.Visible = false;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.AutoSize = true;
+            this.btnExecutar.BackColor = System.Drawing.SystemColors.Control;
+            this.btnExecutar.Image = global::DownloadBRLogic.Properties.Resources.iniciar;
+            this.btnExecutar.Location = new System.Drawing.Point(1132, 2);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(157, 47);
+            this.btnExecutar.TabIndex = 7;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnExecutar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExecutar.UseVisualStyleBackColor = false;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // lblSelecao
+            // 
+            this.lblSelecao.AutoSize = true;
+            this.lblSelecao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(0)))), ((int)(((byte)(114)))));
+            this.lblSelecao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelecao.ForeColor = System.Drawing.Color.White;
+            this.lblSelecao.Location = new System.Drawing.Point(28, 12);
+            this.lblSelecao.Name = "lblSelecao";
+            this.lblSelecao.Size = new System.Drawing.Size(390, 25);
+            this.lblSelecao.TabIndex = 3;
+            this.lblSelecao.Text = "Download BRLogic - Paulo Pimentel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1291, 853);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Download Músicas BRLogic";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblSelecao;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Label lblProgresso;
+        private System.Windows.Forms.Label lblInicio;
+    }
+}
+
---
+++ DownloadBRLogic/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace DownloadBRLogic
+{
+    public partial class Form1 : Form
+    {
+        string path = @"c:\MusicasBRLogic\";
+        object token = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(path))
+            {
+                System.IO.Directory.CreateDirectory(path);
+                MessageBox.Show("Diretório: " + path + " criado com sucesso.", "Atenção!", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                token = webBrowser1.Document.InvokeScript("eval", new object[] { "RFM_AUTH_PARAM" });
+
+                var structureFather = webBrowser1.Document.GetElementById("radio-file-manager");
+                var structureFiles = structureFather.Children[4].Children[0].Children;
+                HtmlElementCollection structureDirectory = null;
+                string subPath = string.Empty;
+                int qtdMusic = structureFiles.Count;
+                int musicDownload = 0;
+                DateTime startDownload = DateTime.Now;
+                DateTime endDownload = DateTime.Now;
+
+                foreach (HtmlElement item in structureFiles)
+                {
+                    if (item.Id.Contains("folder"))
+                    {
+                        qtdMusic--;
+                        continue;
+                    }
+
+                    if (structureDirectory == null)
+                    {
+                        structureDirectory = structureFather.Children[3].Children[1].GetElementsByTagName("a");
+                        string directory = structureDirectory[structureDirectory.Count - 1].InnerText;
+                        subPath = System.IO.Path.Combine(path, directory);
+
+                        if (!System.IO.Directory.Exists(path + @"\" + directory))
+                        {
+                            System.IO.Directory.CreateDirectory(subPath);
+                            MessageBox.Show("Sub Diretório: " + subPath + " criado com sucesso.", "Atenção!", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Information);
+                            startDownload = DateTime.Now;
+                        }
+                    }
+
+                    string identify = item.GetAttribute("data-id");
+                    string dirMusicFull = subPath + @"\" + item.GetElementsByTagName("a")[0].InnerText + ".mp3";
+                    client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
+
+                    if (System.IO.File.Exists(dirMusicFull))
+                    {
+                        continue;
+                    }
+
+                    musicDownload++;
+
+                    this.lblInicio.Visible = true;
+                    this.lblInicio.Text = "Iniciado às " + startDownload.ToString("HH:mm tt");
+                    this.lblInicio.Refresh();
+
+                    this.lblProgresso.Visible = true;
+                    this.lblProgresso.Text = "Baixando " + musicDownload.ToString() + " de " + qtdMusic.ToString() + " músicas";
+                    this.lblProgresso.Refresh();
+
+                    client.DownloadFile(@"https://servidor23.brlogic.com/index.php/163212/radio-file-manager/download?" + token + "&file=" + identify, dirMusicFull);
+                }
+
+                if (musicDownload > 0)
+                {
+                    endDownload = DateTime.Now;
+                    TimeSpan duracao = endDownload - startDownload;
+                    StreamWriter sw = File.AppendText(subPath + @"\Log.txt");
+                    sw.WriteLine
+                        (
+                            "Download iniciado às " + startDownload + " e finalizado às " + endDownload +
+                            " | Duração do download: " + duracao.ToString(@"hh\:mm\:ss") +
+                            " | " + musicDownload + " músicas baixadas."
+                        );
+                    sw.Close();
+                    MessageBox.Show("Download finalizado com sucesso.\n\nVocê baixou " + musicDownload + " músicas em " + duracao.ToString(@"hh\:mm\:ss") + ".", "Atenção!", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Não foi baixado nenhuma música, verifique se você carregou a página toda até o rodapé!", "Atenção!", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Exclamation);
+                }
+
+                this.lblProgresso.Text = "";
+                this.lblProgresso.Visible = false;
+                this.lblInicio.Text = "";
+                this.lblInicio.Visible = false;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Provávelmente você não está na página de repositório de música, favor acesse e tente novamente!", "ERRO!", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
+
+        }
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+    }
+}
